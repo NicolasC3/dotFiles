faa6513ee471b78ed6ee43d082730b8b2f02f371
NicolasC3 <nicolasc3@gmail.com>
2023-02-18 15:43:53 +0100

    Correct install script

--- nvimConfigUpdate.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-
-git pull
-mkdir $HOME/.config/nvim
-cp ./nvimConfig/* $HOME/.config/nvim
--- nvimInstall.sh
+++ nvimInstall.sh
@@ -10,7 +10,11 @@ then
 	mkdir $installDir
 fi
 
-cp ./nvim.appimage $installDir/nvim
+if [ -f "$installDir/nvim"  ];
+then
+    rm $installDir/nvim
+fi
+cp -s $(pwd)/nvim.appimage $installDir/nvim
 chmod +x $installDir/nvim
 
 if ! [[ :$PATH: == *:$installDir:*  ]];
@@ -24,12 +28,15 @@ source $HOME/.bashrc
 echo "Do you want to init neovim config (this operation will IRREMEDIABLY discard any previous neovim configuration) ? (y/n)"
 read init
 
+nvim &
+kill -9 $!
+
 if [ "$init" = "y" ]; then
 	rm -rf $HOME/.config/nvim $HOME/.local/share/nvim $HOME/.local/state/nvim
-	
+
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 	mkdir $HOME/.config/nvim
-	cp ./nvimConfig/* $HOME/.config/nvim
+	cp -rs $(pwd)/nvimConfig/* $HOME/.config/nvim
 fi
