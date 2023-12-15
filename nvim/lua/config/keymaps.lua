ec86b6e2937afdea98b88927013d662c70000668
CATONI Nicolas <ncatoni@knuth.enseeiht.fr>
2023-12-15 21:09:29 +0100

    harpoon

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- HARPOON
+
+local harpoon = require("harpoon")
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():append()
+end)
+vim.keymap.set("n", "<C-a>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<leader>h", function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<leader>j", function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<leader>k", function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<leader>l", function()
+  harpoon:list():select(4)
+end)
---
+++ nvim/lua/plugins/custom_plug.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    lazy = false,
+    requires = { { "nvim-lua/plenary.nvim" } },
+    branch = "harpoon2",
+    config = function()
+      require("harpoon").setup()
+    end,
+  },
+}
